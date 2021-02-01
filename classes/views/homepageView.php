6d2bb076e40ce480c1d0d5cbcb68127ce021c2dc
Anthonyvago <anthonyvago2001@gmail.com>
2021-02-02 00:06:17 +0100

    new feature, always 5 stars

--- classes/views/homepageView.php
+++ classes/views/homepageView.php
@@ -95,12 +95,26 @@ $gemRating = calcAvarageRating($googleReviews);
                                     <a href="https://www.google.com/maps/contrib/<?= $googleReview['accountId'] ?>/reviews"
                                        target="_blank"><?= $googleReview['name'] ?></a>
                                     <div class="row">
-                                        <?php for ($i = 0; $i < $googleReview['stars']; $i++) { ?>
-                                            <svg width="19px" height="19px" viewBox="0 0 16 16" class="bi bi-star-fill"
-                                                 fill="rgb(18, 49, 100)" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-                                            </svg>
-                                        <?php }
+                                        <?php
+                                        $nrOfMaxStars = 5;
+                                        $drawStar = true;
+
+                                        for ($starNr = 0; $starNr < $nrOfMaxStars; $starNr++) {
+                                            if ($starNr >= $googleReview['stars'] && $drawStar == true) {
+                                                $drawStar = false;
+                                            }
+                                            if ($drawStar) {
+                                                echo '<svg width="19px" height="19px" viewBox="0 0 16 16" class="bi bi-star-fill"
+                                                    fill="rgb(18, 49, 100)" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                                                    </svg>';
+                                            } else {
+                                                echo '<svg width="19px" height="19px" viewBox="0 0 16 16" class="bi bi-star"
+                                                    fill="rgb(18, 49, 100)" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+                                                    </svg>';
+                                            }
+                                        }
                                         echo '<a href="https://www.google.com/maps/contrib/' . $googleReview['accountId'] . '/reviews" class="aant-sterren" target="_blank">(' . $googleReview['stars'] . ')</a>'; ?>
                                     </div>
                                 </div>
