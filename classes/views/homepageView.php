94d8783fc7f93cbd7e51e12c72daa817a09f9b6a
Anthonyvago <anthonyvago2001@gmail.com>
2021-02-02 02:22:38 +0100

    User can now see exact date and small code-improvements

--- classes/views/homepageView.php
+++ classes/views/homepageView.php
@@ -64,7 +64,7 @@ $gemRating = calcAvarageRating($googleReviews);
                 </div>
                 <div class="body">
                     <?php
-                    echo '<p>' . number_format($gemRating, 1) . ' / 5.0</p>';
+                    echo '<p>' . number_format($gemRating, 2) . ' / 5.00</p>';
                     $newGemRating = round($gemRating * 2) / 2; /* Rondt af op halve getallen */
                     $starSize = 22;
                     for ($i = 0; $i < floor($newGemRating); $i++) { ?>
@@ -103,17 +103,16 @@ $gemRating = calcAvarageRating($googleReviews);
                                             if ($starNr >= $googleReview['stars'] && $drawStar == true) {
                                                 $drawStar = false;
                                             }
+                                            echo '<svg width="19px" height="19px" viewBox="0 0 16 16" class="bi bi-star';
+                                            if ($drawStar): echo 'class="bi bi-star-fill"'; endif;
+                                            if (!$drawStar): echo 'class="bi bi-star"'; endif;
+                                            echo 'fill="rgb(18, 49, 100)" xmlns="http://www.w3.org/2000/svg">';
                                             if ($drawStar) {
-                                                echo '<svg width="19px" height="19px" viewBox="0 0 16 16" class="bi bi-star-fill"
-                                                    fill="rgb(18, 49, 100)" xmlns="http://www.w3.org/2000/svg">
-                                                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-                                                    </svg>';
+                                                echo '<path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>';
                                             } else {
-                                                echo '<svg width="19px" height="19px" viewBox="0 0 16 16" class="bi bi-star"
-                                                    fill="rgb(18, 49, 100)" xmlns="http://www.w3.org/2000/svg">
-                                                    <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
-                                                    </svg>';
+                                                echo '<path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>';
                                             }
+                                            echo '</svg>';
                                         }
                                         echo '<a href="https://www.google.com/maps/contrib/' . $googleReview['accountId'] . '/reviews" class="aant-sterren" target="_blank">(' . $googleReview['stars'] . ')</a>'; ?>
                                     </div>
@@ -128,14 +127,10 @@ $gemRating = calcAvarageRating($googleReviews);
                                            style="background-color: var(--saab-color2); background-image: url('<?= $image['image'] ?>')"></a>
                                     <?php } ?>
                                 </div>
-                            <?php } ?>
-                            <p class="datum text-right">
-                                <?php
-                                $timeDiffArr = getTimeDifference($googleReview['date']);
-                                $timeDiffString = getStringToDisplay($timeDiffArr);
-                                echo $timeDiffString;
-                                ?>
-                            </p>
+                            <?php }
+                            echo '<p class="datum text-right">' . getTimeDiffToString($googleReview['date']) . '</p>';
+                            echo '<p class="datum text-right">' . getDateToText($googleReview['date']) . '</p>';
+                            ?>
                         </div>
                     </div>
                 <?php } ?>
--- includes/homepageFunctions.php
+++ includes/homepageFunctions.php
@@ -16,7 +16,7 @@ function calcAvarageRating($googleReviews)
     return $countRating / $countReviews;
 }
 
-function getTimeDifference($date1)
+function getTimeDiffToString($date1)
 {
     $date2 = date('Y/m/d'); //Pakt de datum van vandaag
     $timeDifference = abs(strtotime($date2) - strtotime($date1)); /* Aantal sec verschil tussen vandaag en wnnr review is geplaatst */
@@ -25,27 +25,20 @@ function getTimeDifference($date1)
     $months = floor(($timeDifference - $years * 365 * 60 * 60 * 24) / (30 * 60 * 60 * 24));
     $days = floor(($timeDifference - $years * 365 * 60 * 60 * 24 - $months * 30 * 60 * 60 * 24) / (60 * 60 * 24));
 //  Afronden op maanden:
-    if ($days > 15) { /* Afronden op maanden; Als het meer dan een halve maand scheelt, gaat het aantal van maanden omhoog */
+    $date1 = strtotime($date1);
+    if ($days > cal_days_in_month(CAL_GREGORIAN, date("m", $date1), date("Y", $date1))) { /* Afronden op maanden; Als het meer dan een halve maand scheelt, gaat het aantal van maanden omhoog */
         $months++;
-        if ($months > 12) { /* Als $months 13 of hoger is, reset ie naar 0 (= januari) en komt er een jaar bij */
+        if ($months > 12) { /* Als $months hoger is dan 12, reset ie naar 0 (= januari) en komt er een jaar bij */
             $months = 0;
             $years++;
         }
     }
-    return [$years, $months, $days];
-}
-
-function getStringToDisplay($timeDiffArr)
-{
-    $years = $timeDiffArr[0];
-    $months = $timeDiffArr[1];
-    $days = $timeDiffArr[2];
-//  Genereert de string die de gebruiker moet zien
+    //  Genereert de string die de gebruiker moet zien
     $timeDiffString = '';
     if ($years > 0) {
         if ($months == 0) $timeDiffString = 'Ongeveer ' . $years . ' jaar geleden';
         if ($months == 1) $timeDiffString = 'Ongeveer ' . $years . ' en ' . $months . ' maand geleden';
-        if ($months > 1) $timeDiffString = 'Ongeveer ' . $years . ' en ' . $months . ' maanden geleden';
+        if ($months > 1) $timeDiffString = 'Ongeveer ' . $years . ' jaar en ' . $months . ' maanden geleden';
     } else {
         if ($months == 0) {
             if ($days == 0) $timeDiffString = 'Vandaag';
@@ -56,4 +49,23 @@ function getStringToDisplay($timeDiffArr)
         if ($months > 1) $timeDiffString = 'Ongeveer ' . $months . ' maanden geleden';
     }
     return $timeDiffString;
+}
+
+function getDateToText($reviewDate)
+{
+    $reviewDate = strtotime($reviewDate);
+    $reviewDay = date("d", $reviewDate);
+    $reviewMonth = date("m", $reviewDate);
+    $reviewYear = date("Y", $reviewDate);
+
+    $allMonthsArr = ['januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december'];
+    for ($monthNr = 0; $monthNr < count($allMonthsArr); $monthNr++) {
+        if ($monthNr + 1 == $reviewMonth) {
+            $reviewMonth = $allMonthsArr[$monthNr];
+        }
+    }
+
+    $reviewDay = $reviewDay * 10 / 10; // Als deze line er niet was, dan zou er komen te staan: "04 juni 2020". Van die "04" willen wij natuurlijk "4" maken.
+
+    return '(' . $reviewDay . ' ' . $reviewMonth . ' ' . $reviewYear . ')';
 }
